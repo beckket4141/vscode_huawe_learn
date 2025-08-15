eeb7ae5c9581a81f308470dc9cbb0f794694a545
beckket4141 <a13667491861@126.com>
2025-08-15 22:04:09 +0800

    图的BFS

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-strs=list(map(str,input().split()))
-print(*strs)
+n = int(input())
+nums = list(map(int, input().split()))
+total = int(input())
+
+# 如果总和是奇数，直接返回0
+target = total
+    # 初始化dp数组，dp[j]表示和为j的方案数
+dp = [0] * (target + 1)
+dp[0] = 1  # 空子集的和为0，方案数为1
+    
+for num in nums:
+    # 从target逆向遍历到num，避免重复使用当前元素
+    for j in range(target, num - 1, -1):
+        dp[j] += dp[j - num]
+    
+print(dp[target])
