f8a15334349485efc6e1abd8178017a136914ec8
beckket4141 <a13667491861@126.com>
2025-08-20 23:47:54 +0800

    8.20

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-from collections import deque
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    m, n = int(data[0]), int(data[1])
+    grid = [[] for _ in range(m)]
+    idx = 2
+    hole = []
+    for i in range(m):
+        row = data[idx]
+        grid[i] = list("".join(row))
+        idx += 1
 
-a = 1
-b = 2
-q = deque([a])
-q.append(b)
-print(q)
+    print(grid)
+if __name__ == "__main__":
+    main()
