6d7509eaeeb35f9bf7ff823d4609077f96681cb4
beckket4141 <a13667491861@126.com>
2025-08-24 00:16:56 +0800

    二进制枚举

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-arry = [5,2,9,1]
-arry.sort(reverse= True)
-print(arry)
+import sys
+def main():
+    input = sys.stdin.read
+    data = list(map(int, input().split()))
+    seatn, stan, usern = data[0], data[1], data[2]
+    idx = 3
+    userbook = [[]for _ in range(usern)]
+    for i in range(usern):
+        up, down = data[idx], data[idx+1]
+        userbook[i].append((up,down))
+        idx += 2
+    print(userbook)
+
+if __name__ == "__main__":
+    main()
