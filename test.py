1c93140ffeed0a2fbd7904e576f05b7822993e63
beckket4141 <a13667491861@126.com>
2025-08-24 23:25:06 +0800

    回溯添加进入节点和出节点的append以及pop操作都要在for循环下进行!

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-import sys
-def main():
-    input = sys.stdin.read
-    data = list(map(int, input().split()))
-    seatn, stan, usern = data[0], data[1], data[2]
-    idx = 3
-    userbook = [[]for _ in range(usern)]
-    for i in range(usern):
-        up, down = data[idx], data[idx+1]
-        userbook[i].append((up,down))
-        idx += 2
-    print(userbook)
-
-if __name__ == "__main__":
-    main()
+a = ["a","b"]
+b = str("".join(a))
+print(b)
