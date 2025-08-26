81802f4440255798abf46faa484125f35f6abf6e
becekket4141 <a13667401861@126.com>
2025-08-26 17:43:47 +0800

    一些python语法规则

--- test.py
+++ test.py
@@ -1,18 +1,3 @@
-import sys
-sys.setrecursionlimit(1000000000)
-def main():
-    input = sys.stdin.read
-    data = list(input().split())
-    m, n = int(data[0]), int(data[1])
-    idx = 2
-    grid = [[""]*n for _ in range(m)]
-    for i in range(m):
-        for j in range(n):
-            grid[i][j] = data[idx]
-            idx += 1
-    target = data[idx]
-    print(grid)
-    print(target)
-
-if __name__ == "__main__":
-    main()
+n = 5
+grid = ['.'*n for _ in range(n)]
+print(grid)
