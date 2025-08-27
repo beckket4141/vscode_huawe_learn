2a88c222c9513a2b82710dcfabc491cd44489312
beckket4141 <a13667491861@126.com>
2025-08-27 21:46:10 +0800

    最大区间重叠度问题

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-a = [1,2]
-a = [3,4]
-print(a)
+import sys
+def main():
+    m, n = map(int,sys.stdin.readline().split())
+    grid = [[] for _ in range(m)]
+    for i in range(m):
+        grid[i] = list(map(int,sys.stdin.readline().split()))
+
+    print(grid)
+if __name__ == '__main__':
+    main()
