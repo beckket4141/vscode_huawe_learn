e10a7b216c56f1104a3c956a32a134029f839e5b
beckket4141 <a13667491861@126.com>
2025-09-05 21:27:26 +0800

    二进制运算

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
+
+
 n = int(input().strip())
 areas = [[] for _ in range(n)]
 for i in range(n):
     s, e = map(str, input().strip("[]").split(","))
-    sl, el = list(map(int, s.split("."))), list(map(int,e.split(".")))
+    sl, el = list(map(int, s.split("."))), list(map(int, e.split(".")))
     areas[i] = [sl, el]
 
+areas.sort(key = lambda x : (x[0][0], x[0][1], x[0][2], x[0][3]) )
 print(areas)
